b14221aec2bf14022f23442cc3adcd1367ff7bec
ayub momin <AYOUB@codemastr.com>
2019-05-30 20:06:48 +0530

    some changes in home page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,19 @@
 // applying background color a fixed navigation menu 
 
-document.getElementById('#fullpage').ready("change",function(e) {
+document.getElementById('#fullpage').ready("change", function(e) {
     var dropdown = document.getElementById('mainNav');
     window.location.href = dropdown.options[dropdown.selectedIndex].getAttribute('id') + '.html';
 });
 
 
 wrap.on("scroll", function(e) {
-    
-  if (this.scrollTop > 2) {
-      wrap.removeClass("trans-navigation")
-    wrap.addClass("position-fixed w-100 bg-white color-black");
-  } else {
-    wrap.removeClass("position-fixed w-100 bg-white color-black");
-  }
-  
+
+    if (this.scrollTop > 2) {
+        wrap.removeClass("trans-navigation")
+        wrap.addClass("position-fixed w-100 bg-white color-black");
+        wrpa.style("z-index:9999;");
+    } else {
+        wrap.removeClass("position-fixed w-100 bg-white color-black");
+    }
+
 });
